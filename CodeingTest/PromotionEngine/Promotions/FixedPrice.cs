b7915465786839f65e10a85cd77fbd433908ab22
Christian Faxholm <c.faxholm@gmail.com>
2021-10-23 17:02:36 +0200

    Added test suite directy to FixedPrice class, before implemtation.

---
+++ CodeingTest/PromotionEngine/Promotions/FixedPrice.cs
@@ -0,0 +1,16 @@
+﻿using PromotionEngine.Interfaces;
+using PromotionEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Promotions
+{
+    public class FixedPrice : IPromtion
+    {
+        public double UsePromotion(List<CartItem> cartItems)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodeingTest/PromotionEngineTests/Promotions/FixedPriceTests/UsePromotion.cs
@@ -0,0 +1,165 @@
+﻿using PromotionEngine.Models;
+using PromotionEngine.Promotions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace PromotionEngineTests.Promotions.FixedPriceTests
+{
+    public class UsePromotion
+    {
+        [Fact]
+        public void One_item()
+        {
+            //Arrange
+            var fixedPricePromotion = new FixedPrice();
+
+            var inputCartItems = new List<CartItem>()
+            {
+                new CartItem() { SKUId = "A", Amount = 1 }
+            };
+
+            var expectedOutput = 0;
+
+            //Act
+            var ouput = fixedPricePromotion.UsePromotion(inputCartItems);
+
+            //Assert
+            Assert.Equal(expectedOutput, ouput);
+        }
+
+        [Fact]
+        public void No_amount_both()
+        {
+            //Arrange
+            var fixedPricePromotion = new FixedPrice();
+
+            var inputCartItems = new List<CartItem>()
+            {
+                new CartItem() { SKUId = "A" },
+                new CartItem() { SKUId = "B" }
+            };
+
+            var expectedOutput = 0;
+
+            //Act
+            var ouput = fixedPricePromotion.UsePromotion(inputCartItems);
+
+            //Assert
+            Assert.Equal(expectedOutput, ouput);
+        }
+
+        [Fact]
+        public void No_amount_one()
+        {
+            //Arrange
+            var fixedPricePromotion = new FixedPrice();
+
+            var inputCartItems = new List<CartItem>()
+            {
+                new CartItem() { SKUId = "A", Amount = 1 },
+                new CartItem() { SKUId = "B" }
+            };
+
+            var expectedOutput = 0;
+
+            //Act
+            var ouput = fixedPricePromotion.UsePromotion(inputCartItems);
+
+            //Assert
+            Assert.Equal(expectedOutput, ouput);
+        }
+
+        [Fact]
+        public void No_SKU_Id_both()
+        {
+            //Arrange
+            var fixedPricePromotion = new FixedPrice();
+
+            var inputCartItems = new List<CartItem>()
+            {
+                new CartItem() { Amount = 1 },
+                new CartItem() { Amount = 1 }
+            };
+
+            var expectedOutput = 0;
+
+            //Act
+            var ouput = fixedPricePromotion.UsePromotion(inputCartItems);
+
+            //Assert
+            Assert.Equal(expectedOutput, ouput);
+        }
+
+        [Fact]
+        public void No_SKU_Id_one()
+        {
+            //Arrange
+            var fixedPricePromotion = new FixedPrice();
+
+            var inputCartItems = new List<CartItem>()
+            {
+                new CartItem() { SKUId = "A", Amount = 1 },
+                new CartItem() { Amount = 1 }
+            };
+
+            var expectedOutput = 0;
+
+            //Act
+            var ouput = fixedPricePromotion.UsePromotion(inputCartItems);
+
+            //Assert
+            Assert.Equal(expectedOutput, ouput);
+        }
+
+        [Fact]
+        public void Extra_amount_for_one()
+        {
+            //Arrange
+            var fixedPricePromotion = new FixedPrice();
+
+            var inputCartItems = new List<CartItem>()
+            {
+                new CartItem() { SKUId = "A", Amount = 2 },
+                new CartItem() { SKUId = "B", Amount = 1 }
+            };
+
+            var expectedOutput = 70;
+            var expectedAmountForA = 1;
+
+            //Act
+            var ouput = fixedPricePromotion.UsePromotion(inputCartItems);
+
+            //Assert
+            Assert.Equal(expectedOutput, ouput);
+            Assert.Equal(expectedAmountForA, inputCartItems.Single(x => x.SKUId == "A").Amount);
+        }
+
+        [Fact]
+        public void Extra_amount_for_two()
+        {
+            //Arrange
+            var fixedPricePromotion = new FixedPrice();
+
+            var inputCartItems = new List<CartItem>()
+            {
+                new CartItem() { SKUId = "A", Amount = 2 },
+                new CartItem() { SKUId = "B", Amount = 2 }
+            };
+
+            var expectedOutput = 140;
+            var expectedAmountForA = 0;
+            var expectedAmountForB = 0;
+
+            //Act
+            var ouput = fixedPricePromotion.UsePromotion(inputCartItems);
+
+            //Assert
+            Assert.Equal(expectedOutput, ouput);
+            Assert.Equal(expectedAmountForA, inputCartItems.Single(x => x.SKUId == "A").Amount);
+            Assert.Equal(expectedAmountForB, inputCartItems.Single(x => x.SKUId == "B").Amount);
+        }
+    }
+}
