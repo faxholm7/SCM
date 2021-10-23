d03d8f78a4cb1ed4a8275644f598c6f39a11f325
Christian Faxholm <c.faxholm@gmail.com>
2021-10-23 18:13:21 +0200

    Added start of Percentage promotion tests.

---
+++ CodeingTest/PromotionEngineTests/Promotions/PercentageTests/UsePromotion.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PromotionEngineTests.Promotions.PercentageTests
+{
+    public class UsePromotion
+    {
+        [Fact]
+        public void Happy_path()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void Missing_unit_price()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void No_percentage()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void No_SKU_Id()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void Two_items()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.True(false);
+        }
+
+    }
+}
